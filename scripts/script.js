7de09bd325f03edac1b23f7011d178f2a1617dfe
Salim Qodirov <salim.qodirov@digitalinsight.com>
2018-01-19 15:19:41 -0800

    Fix Reset button issue.

--- scripts/script.js
+++ scripts/script.js
@@ -4,12 +4,7 @@ const h4 = document.querySelector("h4");
 const h2 = document.querySelector("h2");
 
 function reset() {
-	const content = document.querySelector("p");
-	humanScore = 0;
-	computerScore = 0;
-	h4.textContent = `Player: ${humanScore} Computer: ${computerScore}`;
-	content.textContent = "";
-	h2.textContent = "";
+	window.location.reload();
 }
 
 function buttonReset() {
